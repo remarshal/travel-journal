ce752e91bd6e89c14ec7271ce6f542737e84b95c
remarshal <marshalwaldendev@gmail.com>
2022-03-28 17:21:25 -0400

    partial card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import Nav from "./Components/Nav"
-import Card from "./Components/Card"
+import Nav from "./Components/Nav";
+import Card from "./Components/Card";
+import data from "./data";
 import './App.css';
 
 function App() {
+  const destinations = data.map(destination => {
+    return (
+      // key={destination.id},
+      destination = { destination }
+    );
+  });
   return (
     <div className="App">
       <Nav />
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,9 @@
 import "./Card.css"
 
-function Card() {
+function Card(props) {
     return (
-        <div>
-            <img />
+        <div className="travel-card">
+            <img src=""/>
             <a>map</a>
             <h2>Country</h2>
             <h1>Location</h1>
@@ -13,4 +13,12 @@ function Card() {
     )
 }
 
-export default Card
+export default Card;
+
+// title: "Mount Fuji",
+// location: "Japan",
+// googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+// startDate: "12 Jan, 2021",
+// endDate: "24 Jan, 2021",
+// description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+// imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
