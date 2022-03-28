c0cb22c18915637023719d5a672c77c4a6857dd0
remarshal <marshalwaldendev@gmail.com>
2022-03-28 17:01:03 -0400

    intial card setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./Components/Nav"
+import Card from "./Components/Card"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Card />
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,16 @@
+import "./Card.css"
+
+function Card() {
+    return (
+        <div>
+            <img />
+            <a>map</a>
+            <h2>Country</h2>
+            <h1>Location</h1>
+            <h5>Date</h5>
+            <p>Description</p>
+        </div>
+    )
+}
+
+export default Card
