3afeeb5d1265ee8d82bcc281e76165a0dbf3f305
remarshal <marshalwaldendev@gmail.com>
2022-04-19 20:06:13 -0400

    made responsive

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,9 +7,12 @@ function Card(props) {
                 <img src={props.destination.imageUrl} className="card--img" />
             </div>
             <div className="travel--title">
-                <h5>{props.destination.location}</h5>
-                <a href={props.destination.googleMapsUrl} target="_blank">View on Google Maps</a>
                 <h1>{props.destination.title}</h1>
+                <h5>{props.destination.location}</h5>
+                <div>
+                <a href={props.destination.googleMapsUrl} target="_blank">View on Google Maps</a>
+                </div>
+                
             </div>
             <div className="travel--details">
                 <h5>{props.destination.startDate} - {props.destination.endDate}</h5>
